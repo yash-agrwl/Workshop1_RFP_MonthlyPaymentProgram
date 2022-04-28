8acab0f890c1eb2e7b9653a4e50ed93488a6da4a
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-28 10:09:40 +0530

    added empty project with welcome message

---
+++ MonthlyPayment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonthlyPayment
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Monthly Payment Program.\n");
+        }
+    }
+}
